e4d0fe774a6ef5e5e5dd40c8ddc02deabdbcc0c8
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-04-20 10:20:06 +0200

    alt desc improved + reveal ani added

---
+++ js/text_reveal.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+window.addEventListener("DOMContentLoaded", start);
+
+const sections = document.querySelectorAll(".text_section");
+
+function start() {
+  console.log("welcome to scroll");
+  revealAnimation();
+}
+
+function revealAnimation() {
+  console.log(sections);
+
+  gsap.utils.toArray(".text_section").forEach((section) => {
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        toggleActions: "restart none none none",
+        start: "top 95%",
+      },
+    });
+
+    tl.to(section, {
+      duration: 1.5,
+      opacity: 1,
+    });
+  });
+}
